6956c9f544ca36d333a63bb5868c60eae41a78da
Hisashi <hisasi622@gmail.com>
2020-10-24 01:18:09 +0900

    passoword表示、非表示切り替え

---
+++ htdocs/js/main.js
@@ -0,0 +1,13 @@
+const pwd = document.getElementById('password');
+const pwdCheck = document.getElementById('password-check');
+pwdCheck.addEventListener(
+  'change',
+  function () {
+    if (pwdCheck.checked) {
+      pwd.setAttribute('type', 'text');
+    } else {
+      pwd.setAttribute('type', 'password');
+    }
+  },
+  false
+);
--- view/indexhtml.php
+++ view/indexhtml.php
@@ -17,13 +17,17 @@
       <input type="text" name="name" placeholder="名前">
       <input type="text" name="furigana" placeholder="ふりがな">
       <input type="text" name="mail" placeholder="メール">
-      <input type="password" name="password" placeholder="パスワード">
+      <input type="password" class="field" id="password" name="password" placeholder="パスワード">
+      <div class="pass" style="display: flex;">
+        <input type="checkbox" id="password-check">
+        <p>パスワードを表示する</p>
+      </div>
       <input type="password" name="verification_for_password" placeholder="確認用パスワード">
-      <select name="sex" id="">
+      <select name="sex">
         <option value="men">男性</option>
         <option value="girl">女性</option>
       </select>
-      <select name="work" id="">
+      <select name="work">
         <option value="worker">社会人</option>
         <option value="student">学生</option>
         <option value="another">その他</option>
@@ -31,6 +35,8 @@
       <button type="submit">確認</button>
     </form>
   </main>
+
+  <script src="js/main.js"></script>
 </body>
 
 </html>
